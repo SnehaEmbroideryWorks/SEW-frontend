b3d32f6f9dfeaf0066c21f2e1e739cfd448581ee
SnehaEmbroideryWorks <orders.snehaembroideryworks@gmail.com>
2026-01-09 21:47:42 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -123,7 +123,7 @@ export default function Cart() {
                 {/* RIGHT */}
                 <div className="text-right">
                   <p className="text-lg font-medium">
-                    ₹{Number(item.basePrice)}
+                    ₹{Number(item.price)}
                   </p>
 
                   <button
@@ -150,6 +150,7 @@ export default function Cart() {
     </div>
   );
 }
+
 
 
 
